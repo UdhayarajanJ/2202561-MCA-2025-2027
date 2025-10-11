cbe105b519f68dbbce5458103fa6e441ffa1e694
Udhayarajan J <udhayarajanj@outlook.com>
2025-10-11 23:52:10 +0530

    [add]: basic style implementation with loader create necessary components

--- MCA-I-MiniProject/first-park-then-pay/src/app/app.ts
+++ MCA-I-MiniProject/first-park-then-pay/src/app/app.ts
@@ -1,11 +1,14 @@
-import { Component, signal } from '@angular/core';
+import { Component,  signal } from '@angular/core';
+import { Header } from './shared/components/header/header';
+import { Menu } from "./shared/components/menu/menu";
 import { RouterOutlet } from '@angular/router';
+import { Loader } from "./shared/components/loader/loader";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header, Menu, RouterOutlet, Loader],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('first-park-then-pay');
