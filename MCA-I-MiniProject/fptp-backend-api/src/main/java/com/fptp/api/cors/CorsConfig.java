1d0f5241352c9e22aa2ad3f0cc9a7a98651e7206
Udhayarajan J <udhayarajanj@outlook.com>
2025-10-18 11:13:39 +0530

    [add]:cors setup with docker angular

--- MCA-I-MiniProject/fptp-backend-api/src/main/java/com/fptp/api/cors/CorsConfig.java
+++ MCA-I-MiniProject/fptp-backend-api/src/main/java/com/fptp/api/cors/CorsConfig.java
@@ -1,20 +1,25 @@
 package com.fptp.api.cors;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Configuration
 public class CorsConfig {
 
+    @Value("${app.cors.allowed-origins:http://localhost:4200}")
+    private String allowedOrigins; // default if not set
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200")); // Angular app
+        config.setAllowedOrigins(Arrays.asList(allowedOrigins.split(","))); // Angular app
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH","OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setAllowCredentials(true);
