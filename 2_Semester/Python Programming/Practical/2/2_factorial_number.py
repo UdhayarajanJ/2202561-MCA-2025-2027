fce7f38f954df6b542ff497a7b41dfbf0122a9a5
Udhayarajan J <udhayarajanj@outlook.com>
2026-05-23 18:03:04 +0530

    remove unnecessary code files

--- 2_Semester/Python Programming/Practical/2/2_factorial_number.py
+++ 2_Semester/Python Programming/Practical/2/2_factorial_number.py
@@ -2,9 +2,9 @@
 
 
 def recursion_factorial_number(n):
-    if n == 0 or n == 1:
-        return n
-    return n * recursion_factorial_number(n - 1)
+    for i in range(1, n):
+        n *= i
+    return n
 
 
 num = int(input("Enter a number: "))
